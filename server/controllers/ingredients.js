b4e5ef212aa8b815e539cb173842891254d0ab0d
GGn0pi <luigi.s1994@gmail.com>
2021-05-02 01:06:03 +0200

    Implements ingredient creation server-side

--- server/controllers/ingredients.js
+++ server/controllers/ingredients.js
@@ -1,4 +1,4 @@
-import ingredientsModel from "../models/ingredient.js";
+import ingredientModel from "../models/ingredient.js";
 
 // Controllers keep the logic separated from the routes
 // This is to make the application more scalable
@@ -6,7 +6,7 @@ import ingredientsModel from "../models/ingredient.js";
 // not exporting as default. it will be imported by name with { }
 export const getIngredients = async (req, res) => {
     try {
-        const ingredientsData = await ingredientsModel.find();
+        const ingredientsData = await ingredientModel.find();
 
         res.status(200).json(ingredientsData);
 
@@ -16,6 +16,16 @@ export const getIngredients = async (req, res) => {
     
 }
 
-export const createIngredient = (req, res) => {
-    res.send('Creating ingredient');
+export const createIngredient = async (req, res) => {
+    const ingr = req.body;
+
+    const newIngredient = new ingredientModel(ingr);
+    
+    try {
+        await newIngredient.save();
+
+        res.status(202).json(newIngredient);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
 }
