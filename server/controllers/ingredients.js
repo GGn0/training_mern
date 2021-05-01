2d000ea07791d41778663bfae2c22e5ce02c829b
GGn0pi <luigi.s1994@gmail.com>
2021-05-02 00:59:47 +0200

    Adds models and ingredient fetching

--- server/controllers/ingredients.js
+++ server/controllers/ingredients.js
@@ -1,7 +1,21 @@
+import ingredientsModel from "../models/ingredient.js";
+
 // Controllers keep the logic separated from the routes
 // This is to make the application more scalable
 
 // not exporting as default. it will be imported by name with { }
-export const getIngredients = (req, res) => {
-    res.send('Working');
+export const getIngredients = async (req, res) => {
+    try {
+        const ingredientsData = await ingredientsModel.find();
+
+        res.status(200).json(ingredientsData);
+
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+    
+}
+
+export const createIngredient = (req, res) => {
+    res.send('Creating ingredient');
 }
--- server/routes/ingredients.js
+++ server/routes/ingredients.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getIngredients } from '../controllers/ingredients.js';
+import { createIngredient, getIngredients } from '../controllers/ingredients.js';
 
 const router = express.Router();
 
 // Callback function when localhost:PORT/ is visited
 router.get('/', getIngredients);
+router.post('/', createIngredient);
 
 export default router;
